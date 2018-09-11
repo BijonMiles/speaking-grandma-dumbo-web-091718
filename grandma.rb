940345e4c9230278b06e7c3708759de4216eccd6
Bijon Miles <bijonmiles01@gmail.com>
2018-09-11 08:28:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,4 +15,9 @@
 def speak_to_grandma(grandma)
   if grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif 
+  elsif grandma == grandma.uppercase
+    puts "NO, NOT SINCE 1938!"
+  else 
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
